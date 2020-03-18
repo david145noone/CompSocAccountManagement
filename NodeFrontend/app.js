7fc969d0be4ff0d86ca729f81d8043393c329cf2
Conor Mc Govern <conor.mcgovern@live.ie>
2020-03-18 19:29:05 +0000

    Authentication now working with LDAP

--- NodeFrontend/app.js
+++ NodeFrontend/app.js
@@ -2,6 +2,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var LdapStrategy = require('passport-ldapauth');
+var expressValidator = require('express-validator');
+const fs = require('fs')
+var req = require('require-yml')
+var conf = req('./config.yml')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -15,10 +23,50 @@ app.set('view engine', 'hbs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
+app.use(session({
+  secret: conf.secret,
+  resave: false,
+  saveUninitialized: false,
+  //cookie: { secure: true }
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
+
+passport.use('ldapauth',
+  new LdapStrategy({
+    server: {
+      url: conf.url,
+      bindDn: conf.bindDn,
+      bindCredentials: conf.bindCredentials,
+      searchBase: conf.searchBase,
+      searchFilter: conf.searchFilter,
+      reconnect: true,
+      tlsOptions: (true) ? {
+        ca: [
+          fs.readFileSync(conf.tlsCertPath)
+        ]
+      } : {}
+    }
+  })
+);
+
+passport.serializeUser(function(user,done){
+  done(null,user);
+});
+
+passport.deserializeUser(function(user,done){
+  done(null,user);
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+});
 
 module.exports = app;
--- NodeFrontend/routes/index.js
+++ NodeFrontend/routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var bodyParser = require("body-parser");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  if (!req.isAuthenticated()) res.redirect('/login');
+  else res.render('index', {title: 'Index'});
+});
+
+router.get('/login', function(req, res) {
+  res.render('login', {title: 'Login'});
+});
+
+router.post('/login', function(req, res, next) {
+  passport.authenticate('ldapauth', function(err, user, info) {
+    if (err) {
+      return next(err); // will generate a 500 error
+    }
+    req.login(user, { session: true }, function (err) {
+      res.redirect('/');
+    });
+  })(req, res, next);
+});
+
+router.post('/logout', function(req, res, next) {
+  req.logout();
+  res.redirect('/');
 });
 
 module.exports = router;
